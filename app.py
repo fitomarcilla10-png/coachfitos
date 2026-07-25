beda1247e74672693f8cc538ad3d04119c0b35c6
fitomarcilla10-png <fitomarcilla10@gmail.com>
2026-07-25 19:09:17 -0300

    Fix import path for create_retrieval_chain

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
